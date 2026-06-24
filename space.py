81c792b836a66363658940aef5522eafa70d7c5a
IanPiraat <IanPiraat@gmail.com>
2026-06-24 17:01:04 +0200

    space

--- space.py
+++ space.py
@@ -7,10 +7,49 @@ p_space = pygame.image.load("images\imagespace\Image20260617164523.png")
 p_space = pygame.transform.scale(p_space,(1600,1000))
 
 p_player1 = pygame.image.load("images\imagespace\Image20260617164516.png")
-p_player1 = pygame.transform.scale(p_player1,(500,413))
+p_player1 = pygame.transform.scale(p_player1,(250,200))
+p_player1 = pygame.transform.rotate(p_player1,270)
 
 p_player2 = pygame.image.load("images\imagespace\Image20260617164520.png")
-p_player2 = pygame.transform.scale(p_player2,(500,413))
+p_player2 = pygame.transform.scale(p_player2,(250,200))
+p_player2 = pygame.transform.rotate(p_player2,90)
+
+
+class player(pygame.sprite.Sprite) :
+    def __init__(self,x,y,color):
+        super().__init__()
+        self.x = x
+        self.y = y 
+        self.color = color
+        if self.color == "red" :
+            self.image = p_player1
+        elif self.color == "yellow" :
+            self.image = p_player2
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x,self.y)
+        self.health = 100
+    def move_ship(self,k)  :
+        if self.color == "red" :
+            if k[pygame.K_w] and self.rect.y > -200 :
+                self.rect.y -= 5 
+            if k[pygame.K_s]  and self.rect.y < 750 : 
+                self.rect.y += 5
+            if k[pygame.K_d] and self.rect.x < 525 :
+                self.rect.x += 5    
+            if k[pygame.K_a] and self.rect.x > -200 :
+                self.rect.x -= 5
+        elif self.color == "yellow" :
+            if k[pygame.K_UP] and self.rect.y > -200:
+                self.rect.y -= 5
+            if k[pygame.K_DOWN] and self.rect.y < 750  :
+                self.rect.y += 5
+            if k[pygame.K_RIGHT] and self.rect.x < 1450 :
+                self.rect.x += 5    
+            if k[pygame.K_LEFT] and self.rect.x > 700 :
+                self.rect.x -= 5
+            
+player1 = player(400,500,"red")
+player2 = player(1200,500,"yellow")
 
 while True :
     screen.fill("white")
@@ -19,9 +58,13 @@ while True :
             pygame.quit()
     screen.blit(p_space,(0,0))
 
-    screen.blit(p_player1,(50,200))
+    screen.blit(player1.image,player1.rect.center)
 
-    screen.blit(p_player2,(600,1400))
+    screen.blit(player2.image,player2.rect.center)
+    pygame.draw.rect(screen,"black",pygame.Rect(800,0,25,1000))
+    keys_pressed = pygame.key.get_pressed()
+    player1.move_ship(keys_pressed)
+    player2.move_ship(keys_pressed)
 
 
     pygame.display.update()
