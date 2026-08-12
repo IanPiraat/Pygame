7985326267b7e76018dbaae6bbae26641bd96b93
IanPiraat <IanPiraat@gmail.com>
2026-08-12 16:58:23 +0200

    finished game

--- jumpyrun.py
+++ jumpyrun.py
@@ -3,6 +3,7 @@ import random
 pygame.init()
 WIDTH =500
 HEIGHT =500
+score = 0
 playerstatus = 1
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 background = pygame.image.load("images/imagesjumpyrun/desertbackground.png")
@@ -10,6 +11,7 @@ background = pygame.image.load("images/imagesjumpyrun/desertbackground.png")
 playeridle = pygame.image.load("images/imagesjumpyrun/playeridle.png")
 playerjump = pygame.image.load("images/imagesjumpyrun/playerjump.png")
 
+font1 = pygame.font.SysFont("Sans Serif",50)
 
 briefcaseimage = pygame.image.load("images/imagesjumpyrun/briefcase.png")
 
@@ -28,8 +30,8 @@ class Player(pygame.sprite.Sprite):
         self.jump_target = self.rect.y
 
     def start_jump(self, charge):
-        charge_seconds = charge / 1000
-        jump_height = max(5, min(int(charge_seconds * 60), 120))
+        charge_seconds = charge / 400
+        jump_height = max(1, min(int(charge_seconds * 500), 240))
         if self.rect.y == self.ground_y:
             self.jump_target = self.ground_y - jump_height
             self.jumping = True
@@ -46,28 +48,28 @@ class Briefcase(pygame.sprite.Sprite):
     def __init__(self, x, image):
         super().__init__()
         self.image = image
+        self.x = x
+        self.y = random.randint(300,500)
         self.rect = self.image.get_rect()
+        self.rect.topleft = self.x,self.y
         self.start_x = x
-        self.reset()
-
-    def reset(self):
-        self.rect.x = self.start_x
-        self.rect.y = random.randint(100, 400)
         self.active = True
 
-    def update(self, player_rect):
-        if not self.active:
-            return
-        if self.rect.centerx < player_rect.centerx:
-            self.rect.x += 3
-        else:
-            self.rect.x -= 3
-        if self.rect.right < 0 or self.rect.left > WIDTH:
-            self.reset()
 
+    def update(self):
+        global score
+        print(self.rect.x,player.rect.x)
+        if self.rect.x > 50 :
+            self.rect.x -= 3
+        else:
+            self.kill()
+            score += 1
+            
     def check_collision(self, player_rect):
+        global score
         if self.active and self.rect.colliderect(player_rect):
-            self.reset()
+            self.kill()
+            score -= 1
             return True
         return False
 
@@ -80,12 +82,22 @@ class Briefcase(pygame.sprite.Sprite):
 
 
 player = Player(100, 400, playeridle)
-briefcase = Briefcase(WIDTH, briefcaseimage)
-start = 0
+playergroup = pygame.sprite.Group()
+briefcasegroup = pygame.sprite.Group()
 
+playergroup.add(player)
+briefcase = None
+start = 0
+count = 0
 while True :
     dt = clock.tick(60)
-    screen.fill("white")
+    count += 1
+    
+    screen.blit(background,(0,0))
+    
+    if count > 0 and count % 300 == 0 :
+        briefcase = Briefcase(WIDTH, briefcaseimage)
+        briefcasegroup.add(briefcase)
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             pygame.quit()
@@ -103,19 +115,25 @@ while True :
             if event.key == pygame.K_s:
                 playerstatus = 0
 
-    player.update()
-    briefcase.update(player.rect)
-    if briefcase.check_collision(player.rect):
-        
 
-        screen.blit(background,(0,0))
-        if briefcase.active:
-            screen.blit(briefcase.image, briefcase.rect)
-        if playerstatus == 1 :
-            screen.blit(playeridle, player.rect)
-        elif playerstatus == 0 :
-            screen.blit(playerjump, player.rect)
+    text1 = font1.render("score = {}".format(score),True,"#c2380e")        
 
+    playergroup.update()
+    screen.blit(text1,(0,0))
+    briefcasegroup.update()
+    for briefcase in briefcasegroup :
+        if briefcase.check_collision(player.rect):
+            
+
+            screen.blit(background,(0,0))
+            if briefcase.active:
+                screen.blit(briefcase.image, briefcase.rect)
+            if playerstatus == 1 :
+                screen.blit(playeridle, player.rect)
+            elif playerstatus == 0 :
+                screen.blit(playerjump, player.rect)
+    playergroup.draw(screen)
+    briefcasegroup.draw(screen)
     pygame.display.update()
 
 
